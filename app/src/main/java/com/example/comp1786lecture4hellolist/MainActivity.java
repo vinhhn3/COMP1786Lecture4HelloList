494942bbf005ea249089aa4d3c40de8de85fd25a
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-27 22:38:15 +0700

    Display the ListView

--- app/src/main/java/com/example/comp1786lecture4hellolist/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture4hellolist/MainActivity.java
@@ -4,12 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
+    private static final String[] WEEKDAYS = new String[]{
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, WEEKDAYS));
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long ig) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView)view).getText(), Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 }
